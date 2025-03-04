9bd657cc1cdfdb01843dd13bd8cfc96802b8dea3
Kenny <kpiccirilliplatt@gmail.com>
2025-03-03 16:32:35 -0800

    Update outputBuffer.cpp
    switched to sstream

--- outputBuffer.cpp
+++ outputBuffer.cpp
@@ -1,24 +1,12 @@
-#include <string>
+#include <sstream>
 
 // buffered output to reduce syscalls
 void bufferOutput(LL<int>& l)
 {
-    string buffer = {};
-    for (auto it = l.begin(); it != nullptr; it++)
-    {
-        //default size is ~1 mil chars
-        if (buffer.size() >= 1000000)
-        {
-            cout << buffer;
-            buffer.clear();
-        }
-        buffer.insert(buffer.size(), to_string(*it));
-
-        // switch this for '\n' if you want... 
-        // it will take longer to print tho
-        buffer += ' ';
+    ostringstream buffer;
+     
+    for (auto it = l.begin(); it != nullptr; it++){
+        buffer << *it << ' ';
     }
-    // print any remaining 
-    cout << buffer;
-    return;
+    cout << buffer.str();
 }
